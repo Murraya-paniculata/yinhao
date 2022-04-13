8a064cca7e0a94e13f747231943cceff46a5f61f
Murraya-paniculata <1395039366@qq.com>
2022-04-14 01:33:58 +0800

    分页组件

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Marquee from './pages/marquee/Marquee';
 import Students from './pages/student/Students';
 // import ContextTest from './pages/context';
-import PagerTest from './components/pagination/PagerTest';
+import PagerTest from './components/pagination';
 
 export const ctx = React.createContext({
   a: ""
