49bf855da4d4f862b4c81c3fb70c10d4bed369d2
Murraya-paniculata <1395039366@qq.com>
2022-04-13 23:49:29 +0800

    上下文的使用

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Marquee from './pages/marquee/Marquee';
 import Students from './pages/student/Students';
+import ContextTest from './pages/context';
+
+export const ctx = React.createContext({
+  a: ""
+});
+export const ctx2 = React.createContext({
+  a: ""
+});
 
 function App() {
   return (
     <div className="App">
-     <Students />
+      <ctx2.Provider value={{a: "上下文2的数据"}}>
+        <ctx.Provider value={{a: "上下文1的数据"}}>
+          <ContextTest />
+        </ctx.Provider>
+      </ctx2.Provider>
     </div>
   );
 }
---
+++ src/pages/context/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ctx, ctx2 } from '../../App'
+
+export default function index(props: any) {
+  return (
+      <div>
+           <ctx.Consumer>
+                {value => {
+                    return <h1>{value.a}</h1>
+                }}
+            </ctx.Consumer>
+            <ctx2.Consumer>
+                {value => {
+                    return <h1>{value.a}</h1>
+                }}
+            </ctx2.Consumer>
+      </div>
+  )
+}
